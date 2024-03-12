2ff7439045a3c21a1c21f24d237a5f353a316886
MedineUrun <medine_urun@hotmail.com>
2024-03-12 21:37:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/workmanagerkullanimi/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.workmanagerkullanimi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.workmanagerkullanimi.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonYap.setOnClickListener {
+            val calismaKosulu = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+          /*  val istek = OneTimeWorkRequestBuilder<MyWorker>()
+                .setInitialDelay(5,TimeUnit.SECONDS)
+                .setConstraints(calismaKosulu)
+                .build()*/
+
+            val istek = PeriodicWorkRequestBuilder<MyWorkerBildirim>(15, TimeUnit.MINUTES)
+                .setInitialDelay(5,TimeUnit.SECONDS)
+                .build()
+
+            WorkManager.getInstance(this).enqueue(istek)
+
+            WorkManager.getInstance(this).getWorkInfoByIdLiveData(istek.id)
+                .observe(this){
+                val durum = it.state.name
+                    Log.e("Arkaplan Durum", durum)
+            }
+        }
+    }
+}
